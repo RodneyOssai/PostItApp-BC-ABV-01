55d2094c35a92a39038147e7bc47f3f5365834c2
Rodney Ossai <ossai2007@gmail.com>
2017-05-30 14:04:05 +0100

    Implementation of Feedback

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # PostItApp-BC-ABV-01
 PostIt is a simple application that allows friends and colleagues create accounts, create groups and add registered users to the groups, and then send messages out to these groups whenever they want.
+# Getting Started
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes. 
+##Prerequisites
+Node<br>
+A git Environment to enable you clone this repo.<br>
+PostMan For Testing - use x-www-formurlencoded parameters.<br>
+#Dependencies
+	"body-parser": "^1.17.2",
+    "express": "^4.15.3",
+    "firebase": "^4.0.0"
+	"eslint": "^3.19.0",
+	"babel-core": "^6.24.1",
+#Deployment
+Navigate to directory of choice on terminal.<br>
+
+Clone this repository on that directory.<br>
+
+Ensure node is installed<br>
+
+Install all app Dependencies<br>
+
+Run the application using npm start<br>
+# Run
+node server.js<br>
+nodemon server.js<br>
+# Heroku
+http://postitbootcamp.herokuapp.com/<br>
+# author
+Rodney Ossai
--- server/postItRouteModule.js
+++ server/postItRouteModule.js
@@ -15,21 +15,21 @@ router.get('/', (req, res) => {
 
 router.route('/user/signup')
 	 //create a user
-  .post((req, res) => {
+	.post((req, res) => {
   		const user = {	username:req.body.username,  // Create and object named users and populate it manually using contents of the document body
 						password:req.body.password, 
 						email:req.body.email 
 					};
 					firebase.auth().createUserWithEmailAndPassword(user.email, user.password) // Create account with firebase using object values
-	.then(userRecord => {        
+		.then(userRecord => {        
 			user.uid = userRecord.uid;  // Push the UserID into the user object
 			let userID = userRecord.uid
 			usersRef.update({[userID] :user}); //Push to the database using uid as key
 			res.send(user) })
-	.catch(error => {
+		.catch(error => {
 			res.json({ message: error.message});
-    })
-  })
+    	})
+	})
 
 router.route('/user/signin')
     //Sign into the application
@@ -45,30 +45,37 @@ router.route('/user/signin')
  })
 
 //Create Broadcast Group
-firebase.auth().onAuthStateChanged(user => {
-	if (user){
-		router.route('/group')
-			.post((req, res) => {
-				let groupName = req.body.groupName;
-				let groupDescription = req.body.groupDescription;
-				let groupCreator = user.uid; //UserID of the group creator goes in here and is pushed in as first member
-				groupsRef.push({name:groupName,Description:groupDescription,members:{groupCreator}});
-				res.send(`You just Successfully created ${groupName}.`)				
-			})
-	}else{
-
-		const message ="Please login to create a group.";
-		message;
-	}
- })
+router.route('/group')
+	.post((req, res) => {
+			let groupName = req.body.groupName;
+			let groupDescription = req.body.groupDescription; 
+			firebase.auth().onAuthStateChanged(user => {
+				if (user){
+					let groupCreator = user.uid;  //UserID of the group creator goes in here and is pushed in as first member	
+					groupsRef.push({name:groupName,Description:groupDescription,members:{groupCreator}});
+					res.json({message:`You just Successfully created ${groupName}.`})			
+				}else{
+					res.send({message:`You must be signed in before you create the group ${groupName}.`})	
+				}
+			})	
+	})
 // Add Members Route
 
 router.route('/group/:groupId/user')
 .post((req, res) => {
   const groupKey = req.params.groupId;
   firebase.database().ref(`group/${groupKey}/members/`)
-  .push({ user: req.body.memberId }); //Get the ID of the member you are adding from Document body
+  .update({ [req.body.memberId]: req.body.memberId }); //Get the ID of the member you are adding from Document body
   res.send('Successfully added user to the group !!');
 });
+//Signout 
+router.route('/signout')
+	.post((req, res) => {
+		firebase.auth().signOut().then(() => {
+		  res.send({message:`You Have Been Sucessfully Signed out`})
+		}).catch(error => {
+		  // An error happened.
+		});
+	})
 
 module.exports = router;
